682edd3064b38ffccbb02ddf4100e6bdc317d584
Diegodev <77650262+dgoldev@users.noreply.github.com>
2023-03-10 14:01:24 -0300

    Update transactions.js

--- js/transactions.js
+++ js/transactions.js
@@ -1,21 +1,20 @@
 const myModal = new bootstrap.Modal("#transaction-modal");
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
+
 let data = {
     transactions: []
 };
 
-checkLogged();
+document.getElementById("button-logout").addEventListener("click", logout);
 
-document.getElementById("logout-button").addEventListener("click", logout);
-
-//ADICIONAR LANCAMENTO
-document.getElementById("transaction-form").addEventListener('submit', function(e) {
+//ADICIONAR LANÇAMENTO
+document.getElementById("transaction-modal").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const value = parseFloat(document.getElementById('value-input').value);
-    const description = document.getElementById('description-input').value;
-    const date = document.getElementById('date-input').value;
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;
+    const date = document.getElementById("date-input").value;
     const type = document.querySelector('input[name="type-input"]:checked').value;
 
     data.transactions.unshift({
@@ -24,13 +23,15 @@ document.getElementById("transaction-form").addEventListener('submit', function(
 
     saveData(data);
     e.target.reset();
-    myModal.hide();
+    myModal.hide();    
 
     getTransactions();
-
-    alert("Lançamento adicionado com sucesso.");
+    
+    alert("Lançamento adicionado com sucesso!");    
 
 });
+
+checkLogged();
 
 function checkLogged() {
     if(session) {
@@ -39,7 +40,7 @@ function checkLogged() {
     }
 
     if(!logged) {
-        window.location.href = 'index.html';
+                window.location.href = "index.html";
         return;
     }
 
@@ -56,18 +57,18 @@ function logout() {
     sessionStorage.removeItem("logged");
     localStorage.removeItem("session");
 
-    window.location.href = 'index.html';
+    window.location.href = "index.html";
 }
 
-function getTransactions() {
-    const transactions = data.transactions;
+function getTransactions () {
+    const transactions = data.transactions
     let transactionsHtml = ``;
 
     if(transactions.length) {
         transactions.forEach((item) => {
             let type = "Entrada";
 
-            if(item.type === 2) {
+            if(item.type === "2") {
                 type = "Saída";
             }
 
@@ -78,11 +79,11 @@ function getTransactions() {
                     <td>${type}</td>
                     <td>${item.description}</td>
                 </tr>
-            `;
-        });
-
-        document.getElementById('transactions-list').innerHTML = transactionsHtml;
+            `
+        }) 
     }
+
+    document.getElementById("transactions-list").innerHTML = transactionsHtml
 }
 
 function saveData(data) {
